95635e1f4ad7a2bef77d882e3022932dc94a45de
이준영 <tera199810@gmail.com>
2025-05-16 11:15:08 +0900

    Initial commit

---
+++ src/main/java/com/example/springjpamemo/SpringJpaMemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpamemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaMemoApplication.class, args);
+    }
+
+}
